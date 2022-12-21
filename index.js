05357a43d8fe1e5a82af6512d323ff4eb8e52859
Alfred Wei <wcmf1129@gmail.com>
2022-12-21 18:35:15 +1300

    change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     var ip = req.socket.remoteAddress;
-    console.log("Just got a request!",ip,"param:",req.params);
+    console.log("Just got a request!",ip,"param:",req.params,"body:",req.body);
     res.send('Yo!')
 })
 app.listen(process.env.PORT || 3000)
