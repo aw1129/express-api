3f5fa6453e80ef56bea54322889cd941f9386f8a
Alfred Wei <wcmf1129@gmail.com>
2022-12-21 18:54:54 +1300

    change

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
+var bodyParser = require('body-parser');
+var useragent = require('express-useragent');
+
+app.use(useragent.express());
+app.use(bodyParser.json());    
+app.use(bodyParser.urlencoded({ extended: true }));    
+
 app.all('/', (req, res) => {
     var ip = req.socket.remoteAddress;
-    console.log("Just got a request!",ip,"param:",req.params,"body:",req.body);
+    console.log("Just got a request!",ip,"param:",req.params,"body:",req.body,"payload:",req.payload);
     res.send('Yo!')
 })
 app.listen(process.env.PORT || 3000)
