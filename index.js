8e0624a49fcf068844bda6fb737e516f15a2be1d
Alfred Wei <wcmf1129@gmail.com>
2022-12-22 17:29:57 +1300

    change

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 var bodyParser = require('body-parser');
 var useragent = require('express-useragent');
+const crypto = require('crypto');
+
+const clickupak = process.env.clickupak;
 
 app.use(useragent.express());
 app.use(bodyParser.json());    
@@ -11,6 +14,17 @@ app.all('/', (req, res) => {
     var ip = req.socket.remoteAddress;
     console.log("Just got a request!",ip,"param:",req.params,"body:");
     console.dir(req.body, { depth: null });
+    console.log("req:");
+    console.dir(req, { depth: null });
     res.send('Yo!')
 })
+
+app.all('/clickup-assign', (req, res) => {
+    var ip = req.socket.remoteAddress;
+    console.log("clickup-assign",ip,"param:",req.params,"body:");
+    console.dir(req.body, { depth: null });
+
+    res.send('Yo!')
+})
+
 app.listen(process.env.PORT || 3000)
